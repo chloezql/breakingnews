c5772f18a9f3a47c092ace5544951e29c4fa48fa
Kathyw15 <54919756+chloezql@users.noreply.github.com>
2025-03-15 23:36:38 -0700

    patch openai package

--- projects/breaking-news-finale/build.sh
+++ projects/breaking-news-finale/build.sh
@@ -6,5 +6,8 @@ npm install --legacy-peer-deps
 # Install sass packages explicitly
 npm install sass node-sass --legacy-peer-deps
 
+# Install openai package explicitly
+npm install openai@4.28.0 --legacy-peer-deps
+
 # Run the build
 npm run build 
